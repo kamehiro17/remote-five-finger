23f08a7ae1143d2680e3f295392cf9845d9c0d86
kamehiro17 <tkameda0317@gmail.com>
2020-06-11 09:19:25 +0900

    add input count

--- imports/ui/facilitator-console.js
+++ imports/ui/facilitator-console.js
@@ -2,35 +2,36 @@ import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { Members } from "../api/collections";
 
-Template.facilitatorConsole.onCreated(function () {
-  console.log("facilitator-console.onCreated");
-  Meteor.subscribe("members");
+Template.facilitatorConsole.onCreated(function() {
+    console.log("facilitator-console.onCreated");
+    Meteor.subscribe("members");
 });
 
 Template.facilitatorConsole.helpers({
-  members() {
-    console.log("Members.find()");
-    console.log(Members.find());
-    return Members.find();
-  },
-  date() {
-      const d = new Date;
-      let formatted = d.getFullYear() + '/';
-      formatted += (d.getMonth() + 1).toString().padStart(2, "0")+'/';
-      formatted += d.getDate().toString().padStart(2, "0")+ ' ';
-      formatted += d.getHours().toString().padStart(2, "0")+ ':';
-      formatted += d.getMinutes().toString().padStart(2, "0");
-      return formatted;
-  },
+    members() {
+        console.log("Members.find()");
+        console.log(Members.find());
+        console.log(Members.find().count());
+        return Members;
+    },
+    date() {
+        const d = new Date;
+        let formatted = d.getFullYear() + '/';
+        formatted += (d.getMonth() + 1).toString().padStart(2, "0") + '/';
+        formatted += d.getDate().toString().padStart(2, "0") + ' ';
+        formatted += d.getHours().toString().padStart(2, "0") + ':';
+        formatted += d.getMinutes().toString().padStart(2, "0");
+        return formatted;
+    },
 });
 
 Template.facilitatorConsole.events({
-  "click #clearBtn"(event) {
-    event.preventDefault();
-    Meteor.call("clear");
-  },
-  "click #openBtn"(event) {
-    event.preventDefault();
-    Meteor.call("open");
-  },
+    "click #clearBtn" (event) {
+        event.preventDefault();
+        Meteor.call("clear");
+    },
+    "click #openBtn" (event) {
+        event.preventDefault();
+        Meteor.call("open");
+    },
 });
