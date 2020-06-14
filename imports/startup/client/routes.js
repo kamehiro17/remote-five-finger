1fcf13a063812b82aae4d225d3abe299c0a4cccc
kamehiro17 <tkameda0317@gmail.com>
2020-06-14 12:05:38 +0900

    add footer, some minor fix

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,35 +5,37 @@ import { BlazeLayout } from "meteor/kadira:blaze-layout";
 import "../../ui/about.html";
 import "../../ui/front.html";
 import "../../ui/facilitator-console.html";
+import "../../ui/_component.html";
+
+const main = document.getElementById("main-container");
 
 FlowRouter.route("/front", {
-  name: "front",
-  action() {
-    require("../../ui/front.js")
-    BlazeLayout.render("front");
-  },
+    name: "front",
+    action() {
+        require("../../ui/front.js")
+        BlazeLayout.render("front", main);
+    },
 });
 
 FlowRouter.route("/about", {
-  name: "about",
-  action() {
-    BlazeLayout.render("about");
-  },
+    name: "about",
+    action() {
+        BlazeLayout.render("about", main);
+    },
 });
 
 FlowRouter.route("/facilitator-console", {
-  name: "facilitator-console",
-  action() {
-    require("../../ui/facilitator-console.js")
-    BlazeLayout.render("facilitatorConsole");
-  },
+    name: "facilitator-console",
+    action() {
+        require("../../ui/facilitator-console.js")
+        BlazeLayout.render("facilitatorConsole", main);
+    },
 });
 
 FlowRouter.route("/", {
-  name: "front",
-  action() {
-    require("../../ui/front.js")
-    BlazeLayout.render("front");
-  },
+    name: "front",
+    action() {
+        require("../../ui/front.js")
+        BlazeLayout.render("front", main);
+    },
 });
-
