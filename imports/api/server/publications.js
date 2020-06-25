55380d05436ede01c119409b62fe8bf29dafdacc
kamehiro17 <tkameda0317@gmail.com>
2020-06-25 09:23:54 +0900

    fix: result sorting

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -2,12 +2,12 @@ import { Meteor } from "meteor/meteor";
 import { Members } from "../collections.js";
 
 Meteor.publish({
-  membersResult: function () {
-    console.log("publish.membersResult called.");
-    return Members.find({ publishFlag: true });
-  },
-  members: function () {
-    console.log("publish.members called.");
-    return Members.find({});
-  },
+    membersResult: function() {
+        console.log("publish.membersResult called.");
+        return Members.find({ publishFlag: true }, { sort: { createdAt: 1 } });
+    },
+    members: function() {
+        console.log("publish.members called.");
+        return Members.find({});
+    },
 });
